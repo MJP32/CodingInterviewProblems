9950855a72324a65425511fdbbaae8d363b99105
michael <michaelperera32@gmail.com>
2019-10-16 00:12:29 -0400

    Initial Commit

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,23 @@
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        PalindromeNumber pn = new PalindromeNumber();
+        System.out.println(pn.isPalindrome3(121));
+        System.out.println(pn.isPalindrome3(-121));
+    }
+
+
+    public static boolean isPalindrome3(int x) {
+        return String.valueOf(x).equals(new StringBuffer(String.valueOf(x)).reverse().toString());
+    }
+    public boolean isPalindrome2(int x) {
+        int sum = 0,target = x;
+        while (x > 0) {
+            int temp = x % 10;
+            x /= 10;
+            sum = sum * 10 + temp;
+        }
+        return sum == target;
+    }
+
+}
