e1dc645595dfe09f176affbd3d36f7e731ff749d
michael <michaelperera32@gmail.com>
2019-11-11 18:50:00 -0500

    Initial Commit

---
+++ src/SpiralMatrix.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println(Arrays.deepToString(matrix));
+    }
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> ls = new ArrayList<>();
+
+       /* 0,0
+        0,1
+        0,2
+        1,2
+        2,2
+        2,1
+        2,0
+        1,0
+        1,1*/
+
+
+        for(int i=0; i <matrix.length;i++){
+            for (int j = 0; j < matrix[0].length; j++) {
+                    ls.add(matrix[i][j]);
+
+            }
+        }
+        return ls;
+    }
+
+}
