a5d1071149ca1d9612e0cac8cb1b08b47d9d31b0
michael <michaelperera32@gmail.com>
2020-10-28 23:52:24 -0400

    Initial

--- src/AlgoExpert/dp/LongestStringChainRecursive.java
+++ src/AlgoExpert/dp/LongestStringChainRecursive.java
@@ -19,7 +19,8 @@ public class LongestStringChainRecursive {
 
     public static void main(String[] args) {
 
-        String[] arr = {"z","zz","zzz","abde", "abc", "abd","zzzz", "abcde", "ade", "ae", "labde","abcdef","e"};
+        String[] arr = {"z","zz","zzz","abde", "abc", "abd","zzzz", "abcde", "ade", "ae", "labde","abcdef","e",
+                "zzzzzzzzzzzzzzzzz"};
         Set<String> set = new HashSet<>();
         for (String a : arr) {
             set.add(a);
@@ -39,17 +40,14 @@ public class LongestStringChainRecursive {
 
     List<String> longestLinks(Set<String> set) {
         Map<String, List<String>> graph = new HashMap<>();
-        String longestString = "";
-        int longestLen = 0;
+
         for (String str : set) {
-            if (str.length() > longestLen) {
-                longestLen = str.length();
-                longestString = str;
-            }
             buildChildren(str, set, graph);
         }
-        Set<String> visited = new HashSet<>();
-        backtrack(longestString, new ArrayList<>(), graph, visited);
+        for(String str: set){
+            Set<String> visited = new HashSet<>();
+            backtrack(str, new ArrayList<>(), graph, visited);
+        }
         return res;
     }
 
